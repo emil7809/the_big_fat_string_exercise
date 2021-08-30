084191916b18ce38206b883de6f593da9714ef1d
emily <emil7809@gmail.com>
2021-08-30 22:21:16 +0200

    first one works!

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 "user strict";
 
+let select;
+let output;
+let input;
+
 window.addEventListener("load", settingUp);
+
+function settingUp(){
+    console.log("settingup");
+    document.querySelector("button").addEventListener("click", generate);
+}
+
+function generate() {
+    console.log("generate");
+
+    input = document.querySelector("#input").value;
+    select = document.querySelector("#select").value;
+
+    if (select === "1"){
+        output = input[0].toUpperCase()+input.slice(1);
+    } else {
+        output = "I made it this far LOVE ME";
+    };
+
+    showOutput();
+}
+
+function showOutput() {
+    console.log("showOutput");
+    document.querySelector("#output").innerHTML = output;
+}
