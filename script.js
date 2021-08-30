03ba134926745ce8c434f4cdfafcaf66e2983c9f
emily <emil7809@gmail.com>
2021-08-30 22:55:08 +0200

    im close to finishing the code one, but not yet

--- script.js
+++ script.js
@@ -19,6 +19,16 @@ function generate() {
 
     if (select === "1"){
         output = input[0].toUpperCase()+input.slice(1);
+    } else if (select === "2"){
+        output = input.substring(input.indexOf(" "), input.substring(input.lastIndexOf(" ")));
+    } else if (select === "3"){
+        output = input.indexOf(" ");
+    } else if (select === "4"){
+        output = input.substring(input.indexOf(" ") +1, input.lastIndexOf(" "));
+    } else if (select === "5"){
+        output = input.endsWith(".jpg");
+    } else if (select === "6"){
+        output = input.indexOf(" "), input.replaceAll(input, `*`);
     } else {
         output = "I made it this far LOVE ME";
     };
